bb82049b4c75ff6f48d0e696229a1f87daf1505f
Jason McEvoy <jason@Jasons-MacBook-Pro.local>
2021-07-27 00:13:27 -0400

    init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gre-essay-practice-tool'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('gre-essay-practice-tool');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('gre-essay-practice-tool app is running!');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable, timer } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+import * as argumentData from '../assets/argument-pool.json';
+import * as issueData from '../assets/issue-pool.json';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'gre-essay-practice-tool';
+
+  counter$: Observable<number> | undefined;
+  count = 60;
+
+  promptType = "issue";
+
+  issueTopics = issueData as Array<any>;
+  argumentTopics = argumentData as Array<any>;
+
+  instructions: string = '';
+  prompt: string = '';
+
+  ngOnInit(): void {
+
+    this.updateTopic();
+
+  }
+
+  updateTopic(): void {
+
+    if (this.promptType === 'issue'){
+
+      console.log("choosing an issue topic");
+      let randomIssueTopic = this.issueTopics[Math.floor(Math.random()*this.issueTopics.length)];
+
+      this.instructions = randomIssueTopic.instructions;
+      this.prompt = randomIssueTopic.prompt;
+
+    }else {
+
+      console.log("choosing an arg topic");
+      let randomArgumentTopic = this.argumentTopics[Math.floor(Math.random()*this.argumentTopics.length)];
+
+      this.instructions = randomArgumentTopic.instructions;
+      this.prompt = randomArgumentTopic.prompt;
+
+    }
+
+    this.counter$ = timer(0,1000).pipe(
+      take(this.count),
+      map(() => --this.count)
+    );
+
+
+  }
+
 }
